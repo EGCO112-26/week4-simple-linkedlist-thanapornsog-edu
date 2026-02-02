cfdb091dea03adefc71bacc3b9644062d217e3e7
6813245_Earth <thanaporn.sog@student.mahidol.edu>
2026-02-02 11:05:29 +0000

    earth

--- node.h
+++ node.h
@@ -4,14 +4,25 @@
 //
 //  Created by Mingmanas Sivaraksa on 4/2/2566 BE.
 //
+#ifndef stdnode_h
+#define stdnode_h
 
-#ifndef node_h
-#define node_h
+#include <string.h> 
 
-struct node
-{
+struct node {
     int value;
     struct node *next;
 };
+
+struct std_node {
+    int id;
+    char name[30];
+    struct std_node *next;
+};
+
+typedef struct node Node;
+typedef Node* NodePtr;
+typedef struct std_node StdNode;
+typedef StdNode* nodePtr;
 
 #endif /* node_h */
