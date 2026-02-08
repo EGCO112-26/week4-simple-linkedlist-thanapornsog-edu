da7375ca5cd0c5d05e418143b228942a16de9641
6813245_Earth <thanaporn.sog@student.mahidol.edu>
2026-02-09 01:12:08 +0700

    Refactor node and std_node structures in node.h
    Updated struct std_node to reduce name size and added new struct node.

--- node.h
+++ node.h
@@ -1,12 +1,28 @@
-#ifndef NODE_H
-#define NODE_H
+//
+//  node.h
+//  simple linkedlist
+//
+//  Created by Mingmanas Sivaraksa on 4/2/2566 BE.
+//
+#ifndef stdnode_h
+#define stdnode_h
+
+#include <string.h> 
+
+struct node {
+    int value;
+    struct node *next;
+};
 
 struct std_node {
     int id;
-    char name[50];
+    char name[30];
     struct std_node *next;
 };
 
-typedef struct std_node* nodePtr;
+typedef struct node Node;
+typedef Node* NodePtr;
+typedef struct std_node StdNode;
+typedef StdNode* nodePtr;
 
-#endif
+#endif /* node_h */
