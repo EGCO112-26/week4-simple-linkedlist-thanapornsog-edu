3c907823a9e332cd5370ec2c2a20eadfb27414c7
6813245_Earth <thanaporn.sog@student.mahidol.edu>
2026-02-09 01:12:38 +0700

    Refactor linked list implementation in main.c

--- main.c
+++ main.c
@@ -1,66 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "node.h"
 
-void insert(nodePtr *head, int id, char *name);
-void print_list(nodePtr head);
-void delete_list(nodePtr *head);
+typedef struct Node {
+    char id[100];
+    char name[100];
+    struct Node* next;
+} Node;
 
-int main(int argc, char **argv) {
-    nodePtr head = NULL;
-    int i;
-
-    if (argc == 1) return 0;
-    if (argc < 3 || (argc % 2) == 0) return 0;
-
-    for (i = 1; i < argc; i += 2) {
-        int id = atoi(argv[i]);
-        char *name = argv[i + 1];
-        insert(&head, id, name);
-    }
-
-    print_list(head);
-
-    delete_list(&head);
-    head = NULL;
-
-    return 0;
+Node* createNode(const char* id, const char* name) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    strcpy(newNode->id, id);
+    strcpy(newNode->name, name);
+    newNode->next = NULL;
+    return newNode;
 }
 
-void insert(nodePtr *head, int id, char *name) {
-    nodePtr new_node = (nodePtr)malloc(sizeof(struct std_node));
-    if (new_node == NULL) return;
-
-    new_node->id = id;
-    strncpy(new_node->name, name, sizeof(new_node->name));
-    new_node->name[sizeof(new_node->name) - 1] = '\0';
-    new_node->next = NULL;
-
+void insert(Node** head, const char* id, const char* name) {
+    Node* newNode = createNode(id, name);
     if (*head == NULL) {
-        *head = new_node;
-        return;
+        *head = newNode;
+    } else {
+        Node* current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
     }
+}
 
-    nodePtr current = *head;
-    while (current->next != NULL) {
+void printList(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        printf("%s %s\n", current->id, current->name);
         current = current->next;
     }
-
-    current->next = new_node;
 }
 
-void print_list(nodePtr head) {
-    nodePtr tmp = head;
-    while (tmp != NULL) {
-        printf("%d %s\n", tmp->id, tmp->name);
-        tmp = tmp->next;
+int main(int argc, char* argv[]) {
+    Node* head = NULL;
+    
+    // Process arguments in pairs (skip argv[0] which is the program name)
+    for (int i = 1; i < argc; i += 2) {
+        if (i + 1 < argc) {
+            insert(&head, argv[i], argv[i + 1]);
+        }
     }
-}
-
-void delete_list(nodePtr *head) {
-    nodePtr tmp;
-    while (*head != NULL) {
+    
+    printList(head);
+    
+    return 0;
+}    while (*head != NULL) {
         tmp = *head;
         *head = (*head)->next;
         free(tmp);
