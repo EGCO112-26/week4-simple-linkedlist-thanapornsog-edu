edbc5bbbcad321f84fdad513376997e82c76cdf2
6813245_Earth <thanaporn.sog@student.mahidol.edu>
2026-02-09 01:00:05 +0700

    Re

--- main.c
+++ main.c
@@ -3,83 +3,74 @@
 #include <string.h>
 #include "node.h"
 
-// Function Prototypes
 void insert(nodePtr *head, int id, char *name);
 void print_list(nodePtr head);
-void delete_list(nodePtr head);
+void delete_list(nodePtr *head);
 
 int main(int argc, char **argv) {
-	nodePtr head = NULL;
-	int i;
+    nodePtr head = NULL;
+    int i;
 
-	// Check for input
-	if (argc < 3) {
-		printf("Usage: %s <ID> <Name> <ID> <Name> ...\n", argv[0]);
-		return 1;
-	}
+    if (argc == 1) {
+        return 0;
+    }
 
-	// Loop through argv in pairs (ID, Name)
-	for (i = 1; i < argc; i += 2) {
-		if (i + 1 < argc) {
-			int id = atoi(argv[i]);
-			char *name = argv[i + 1];
-			insert(&head, id, name);
-		}
-	}
+    if (argc < 3 || (argc % 2) == 0) {
+        return 0;
+    }
 
-	// Print all linked list
-	print_list(head);
+    for (i = 1; i < argc; i += 2) {
+        int id = atoi(argv[i]);
+        char *name = argv[i + 1];
+        insert(&head, id, name);
+    }
 
-	// Delete all linked list (Free memory)
-	delete_list(head);
-	head = NULL;
+    print_list(head);
 
-	return 0;
+    delete_list(&head);
+    head = NULL;
+
+    return 0;
 }
 
-/* --- Functions Implementation --- */
-
-// Insert (sorted by ID ascending)
 void insert(nodePtr *head, int id, char *name) {
-	nodePtr new_node = (nodePtr)malloc(sizeof(struct std_node));
-	if (!new_node) return;
-	new_node->id = id;
-	strncpy(new_node->name, name, sizeof(new_node->name)-1);
-	new_node->name[sizeof(new_node->name)-1] = '\0';
-	new_node->next = NULL;
+    nodePtr new_node = (nodePtr)malloc(sizeof(struct std_node));
+    if (new_node == NULL) return;
 
-	nodePtr current;
+    new_node->id = id;
+    strncpy(new_node->name, name, sizeof(new_node->name));
+    new_node->name[sizeof(new_node->name) - 1] = '\0';
+    new_node->next = NULL;
 
-	// Insert at head
-	if (*head == NULL || (*head)->id > id) {
-		new_node->next = *head;
-		*head = new_node;
-	} else {
-		// Insert in middle or end
-		current = *head;
-		while (current->next != NULL && current->next->id < id) {
-			current = current->next;
-		}
-		new_node->next = current->next;
-		current->next = new_node;
-	}
+    if (*head == NULL || (*head)->id > id) {
+        new_node->next = *head;
+        *head = new_node;
+        return;
+    }
+
+    nodePtr current = *head;
+
+    while (current->next != NULL && current->next->id < id) {
+        current = current->next;
+    }
+
+    new_node->next = current->next;
+    current->next = new_node;
 }
 
-// Print
 void print_list(nodePtr head) {
-	nodePtr tmp = head;
-	while (tmp != NULL) {
-		printf("%d %s\n", tmp->id, tmp->name);
-		tmp = tmp->next;
-	}
+    nodePtr tmp = head;
+    while (tmp != NULL) {
+        printf("%d %s\n", tmp->id, tmp->name);
+        tmp = tmp->next;
+    }
 }
 
-// Delete (free)
-void delete_list(nodePtr head) {
-	nodePtr tmp;
-	while (head != NULL) {
-		tmp = head;
-		head = head->next;
-		free(tmp);
-	}
+void delete_list(nodePtr *head) {
+    nodePtr tmp;
+    while (*head != NULL) {
+        tmp = *head;
+        *head = (*head)->next;
+        free(tmp);
+    }
 }
