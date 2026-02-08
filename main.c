d97666e84919bb032c1956f4a548852a8dc858f6
6813245_Earth <thanaporn.sog@student.mahidol.edu>
2026-02-09 01:33:04 +0700

    1 am

--- main.c
+++ main.c
@@ -11,13 +11,8 @@ int main(int argc, char **argv) {
     nodePtr head = NULL;
     int i;
 
-    if (argc == 1) {
-        return 0;
-    }
-
-    if (argc < 3 || (argc % 2) == 0) {
-        return 0;
-    }
+    if (argc == 1) return 0;
+    if (argc < 3 || (argc % 2) == 0) return 0;
 
     for (i = 1; i < argc; i += 2) {
         int id = atoi(argv[i]);
@@ -42,43 +37,9 @@ void insert(nodePtr *head, int id, char *name) {
     new_node->name[sizeof(new_node->name) - 1] = '\0';
     new_node->next = NULL;
 
-    if (*head == NULL || (*head)->id > id) {
-        new_node->next = *head;
+    if (*head == NULL) {
         *head = new_node;
         return;
-    }
-
-    nodePtr current = *head;
-
-    while (current->next != NULL && current->next->id < id) {
-        current = current->next;
-    }
-
-    new_node->next = current->next;
-    current->next = new_node;
-}
-
-void print_list(nodePtr head) {
-    nodePtr tmp = head;
-    while (tmp != NULL) {
-        printf("%d %s\n", tmp->id, tmp->name);
-        tmp = tmp->next;
-    }
-}
-
-void delete_list(nodePtr *head) {
-    nodePtr tmp;
-    while (*head != NULL) {
-        tmp = *head;
-        *head = (*head)->next;
-        free(tmp);
-    }
-}    while (*head != NULL) {
-        tmp = *head;
-        *head = (*head)->next;
-        free(tmp);
-    }
-}        return;
     }
 
     nodePtr current = *head;
@@ -104,43 +65,4 @@ void delete_list(nodePtr *head) {
         *head = (*head)->next;
         free(tmp);
     }
-}    nodePtr new_node = (nodePtr)malloc(sizeof(struct std_node));
-    if (new_node == NULL) return;
-
-    new_node->id = id;
-    strncpy(new_node->name, name, sizeof(new_node->name));
-    new_node->name[sizeof(new_node->name) - 1] = '\0';
-    new_node->next = NULL;
-
-    if (*head == NULL || (*head)->id > id) {
-        new_node->next = *head;
-        *head = new_node;
-        return;
-    }
-
-    nodePtr current = *head;
-
-    while (current->next != NULL && current->next->id < id) {
-        current = current->next;
-    }
-
-    new_node->next = current->next;
-    current->next = new_node;
 }
-
-void print_list(nodePtr head) {
-    nodePtr tmp = head;
-    while (tmp != NULL) {
-        printf("%d %s\n", tmp->id, tmp->name);
-        tmp = tmp->next;
-    }
-}
-
-void delete_list(nodePtr *head) {
-    nodePtr tmp;
-    while (*head != NULL) {
-        tmp = *head;
-        *head = (*head)->next;
-        free(tmp);
-    }
- }
